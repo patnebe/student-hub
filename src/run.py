081449fc93ad497bbdcd31f1653855248216dc8f
Dev-Nebe <cp.nebe@gmail.com>
2020-05-04 18:42:01 +0100

    chore: Added data models to shell context

--- src/run.py
+++ src/run.py
@@ -1,6 +1,10 @@
 import os
 from .app import create_app, db
 from .app.models.user import User
+from .app.models.project import Project
+from .app.models.nanodegree import Nanodegree
+from .app.models.question import Question, QuestionComment#, CurrentVoteQuestion
+from .app.models.answer import Answer, AnswerComment#, CurrentVoteAnswer
 
 
 app = create_app()
@@ -8,4 +12,15 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {
+        'db': db,
+        'User': User,
+        "Project": Project,
+        "Nanodegree": Nanodegree,
+        'Question': Question,
+        # "Question_Vote": CurrentVoteQuestion,
+        "Question_Comment": QuestionComment,
+        "Answer": Answer,
+        # "Answer_Vote": CurrentVoteAnswer,
+        "Answer_Comment": AnswerComment
+        }
